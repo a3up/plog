b9acdd3b8c655a81063abd74a2c48c929558c3d3
a3brx <a3brx@sapo.pt>
2020-10-06 12:17:30 +0100

    2nd week exercises

--- control-recursion/recursion.pl
+++ control-recursion/recursion.pl
@@ -4,9 +4,9 @@
 factorial(0,1).
 factorial(N,F) :-
 	N > 0,
-	M is N - 1,
-	factorial(M, FM),
-	F is N * FM.
+	N1 is N - 1,
+	factorial(N1, F1),
+	F is N * F1.
 
 % -----------------------------------------------
 %            fibonacci(Number, Result)
@@ -28,13 +28,13 @@ is_prime(2).
 is_prime(3).
 is_prime(N) :-
 	integer(N),
-	P > 3,
-	P mod 2 =\= 0,
-	\+has_factor(P,3).
+	N > 3,
+	N mod 2 =\= 0,
+	\+has_factor(N,3).
 
 % has_factor(Number, Smaller number to test)
 has_factor(N,M) :- 
-	N mod L =:= 0.
+	N mod M =:= 0.
 has_factor(N,M) :-
 	M * M < N,
 	M2 is M + 2,
